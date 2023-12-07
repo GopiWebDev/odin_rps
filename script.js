eff9cab8dc064c9baf3ccfe3a03ca48b907c9a1e
GopiWebDev <gopisv.webdev@gmail.com>
2023-12-07 19:15:21 +0530

    updated js with simple rps game

--- script.js
+++ script.js
@@ -1,60 +1,41 @@
-const options = ["rock", "paper", "scissors"];
-
-function getComputerChoice() {
-  const choice = options[Math.floor(Math.random() * options.length)];
-  return choice;
-}
-
-function checkWinner(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return "Tie";
-  } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")
-  ) {
-    return "Player";
-  } else {
-    return "Computer";
-  }
-}
-
-function playRound(playerSelection, computerSelection) {
-  const result = checkWinner(playerSelection, computerSelection);
-  if (result == "Tie") {
-    return "It's a Tie";
-  } else if (result == "Player") {
-    return "You Won";
-  } else {
-    return "You Lose";
-  }
-}
-
-function getPlayerChoice() {
-  let validatedInput = false;
-  while (validatedInput == false) {
-    const choice = prompt();
-    if (choice == null) {
-      continue;
-    }
-    const choiceInLower = choice.toLowerCase();
-    if (options.includes(choiceInLower)) {
-      validatedInput = true;
-      return choiceInLower;
-    }
-  }
+function computerPlay() {
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * options.length)];
 }
 
 function game() {
-  console.log("Welcome");
   for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
+    function playRound(playerSelection, computerSelection) {
+      if (
+        (playerSelection.toLowerCase() === "rock" &&
+          computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "paper" &&
+          computerSelection === "rock") ||
+        (playerSelection.toLowerCase() === "scissors" &&
+          computerSelection === "paper")
+      ) {
+        return "Player Won";
+      } else if (
+        (playerSelection.toLowerCase() === "rock" &&
+          computerSelection === "paper") ||
+        (playerSelection.toLowerCase() === "paper" &&
+          computerSelection === "scissors") ||
+        (playerSelection.toLowerCase() === "scissors" &&
+          computerSelection === "rock")
+      ) {
+        return "Computer Won";
+      } else if (playerSelection === computerSelection) {
+        return "It's a Tie";
+      } else {
+        return "Invalid Input";
+      }
+    }
+
+    const playerSelection = prompt("Enter anything");
+    const computerSelection = computerPlay();
+    console.log(computerSelection);
     console.log(playRound(playerSelection, computerSelection));
   }
 }
 
 game();
-// const playerSelection = prompt("Enter");
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
