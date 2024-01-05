c151788e2e0763b3099ec8a8f0d2fbae8384fcf3
GopiWebDev <gopisv.webdev@gmail.com>
2024-01-05 19:26:19 +0530

    fully completed rock, paper, scissors game

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
-function computerPlay() {
-  const options = ["rock", "paper", "scissors"];
-  return options[Math.floor(Math.random() * options.length)];
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.querySelector("#playerDisplay");
+const computerDisplay = document.querySelector("#computerDisplay");
+const resultDisplay = document.querySelector("#resultDisplay");
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
+
+// Initial scores of both
+let playerScore = 0;
+let computerScore = 0;
+
+// This function will disable the buttons
+function disableButton(e) {
+  e.disableButton = true;
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    function playRound(playerSelection, computerSelection) {
-      if (
-        (playerSelection.toLowerCase() === "rock" &&
-          computerSelection === "scissors") ||
-        (playerSelection.toLowerCase() === "paper" &&
-          computerSelection === "rock") ||
-        (playerSelection.toLowerCase() === "scissors" &&
-          computerSelection === "paper")
-      ) {
-        return "Player Won";
-      } else if (
-        (playerSelection.toLowerCase() === "rock" &&
-          computerSelection === "paper") ||
-        (playerSelection.toLowerCase() === "paper" &&
-          computerSelection === "scissors") ||
-        (playerSelection.toLowerCase() === "scissors" &&
-          computerSelection === "rock")
-      ) {
-        return "Computer Won";
-      } else if (playerSelection === computerSelection) {
-        return "It's a Tie";
-      } else {
-        return "Invalid Input";
-      }
-    }
+// Main playgame function
+function playGame(playerSelection) {
+  // Randomly chooses 1 value from the array choices
+  const computerSelection = choices[Math.floor(Math.random() * 3)];
+  // Initail result
+  let result = "";
+  // If both selections are same it'll display tie
+  if (playerSelection === computerSelection) {
+    result = "IT'S A TIE";
+  }
+  // all events where player wins
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = "PLAYER WINS";
+  }
+  // if player score reaches 5 it'll disable the button
+  if (playerScore === 5) {
+    disableButton();
+  }
+  // all the events where computer wins
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    result = "COMPUTER WINS";
+  }
+  // if computer score reaches 5 it'll disable the buttons
+  if (computerScore === 5) {
+    disableButton();
+  }
+  // shows the player choice on the display
+  playerDisplay.textContent = `PLAYER: ${playerSelection}`;
+  // shows the computer choice on the display
+  computerDisplay.textContent = `COMPUTER: ${computerSelection}`;
+  // result displays here
+  resultDisplay.textContent = result;
+  // adds classes with color
+  resultDisplay.classList.remove("greenText", "redText");
 
-    const playerSelection = prompt("Enter rock, paper or scissors");
-    const computerSelection = computerPlay();
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
+  switch (result) {
+    // adds 1 if player wins and updates display
+    case "PLAYER WINS":
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    // adds 1 if computer wins
+    case "COMPUTER WINS":
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
   }
 }
 
-game();
+// This function will reload the window i.e refresh the page
+function reset() {
+  window.location.reload();
+}
